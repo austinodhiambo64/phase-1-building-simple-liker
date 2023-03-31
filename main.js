fd3bb6041aa1d8f04f0b2b2d4ed91243525300bf
Austin <austinodhiambo64@gmail.com>
2023-03-31 09:10:35 +0300

    Assignment Completed

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeHearts = document.querySelectorAll('.like-glyph');
 
+function likeCallback(e){
+  const hearts = e.target;
+  mimicServerCall("bogusUrl")
+  .then(function(){
+    if (hearts.innerText === EMPTY_HEART){
+      hearts.innerText = FULL_HEART;;
+      hearts.className = "activated-heart";
+    } else {
+      hearts.innerText = EMPTY_HEART;
+      hearts.className = "";
+    }
+  })
+  .catch(function(error){
+    const modal = document.getElementById("modal");
+    modal.className = "";
+    modal.innerText = error;
+    setTimeout(() => modal.className = "hidden", 3000);
+  })
+}
+for (const glyph of likeHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 
